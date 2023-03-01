7ca6f82884aa120bfac627db65b6ecffd0e6ca82
Have Samuel <samiehave9@gmail.com>
2023-03-01 03:26:25 +0300

    Second Test

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function calculateTaxes(income) {
-    if (!Number.isFinite(income)) {
-        throw new Error('BAD ICOME')
-    }
     if (income > 3000) {
         return income * 0.25
     } else {
         return income * 0.15
     }
+}
+
+function removeDupes(values) {
+    return [...new Set(values)]
 }
--- app.test.js
+++ app.test.js
@@ -3,6 +3,13 @@ it('Should calculate the high tax bracket', function() {
     expect(calculateTax(100000)).toEqual(25000);
 })
 
-it('Should calculate the lower tax bracket', function() {
+it('Should calculate the low tax bracket', function() {
+    expect(calculateTaxes(10000).toEqual(1500));
+    expect(calculateTaxes(100).toEqual(15));
+    expect(calculateTaxes(0).toEqual(0));
+})
 
+it('Should remove duplicates from an array', function() {
+    expect(removeDupes([1, 1, 2, 3])).tobe([1, 2, 3]);
+    expect(removeDupes([1, 2, 3])).tobe([1, 2, 3]);
 })
