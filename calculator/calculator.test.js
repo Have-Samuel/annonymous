f3c3ad4aa6c172e2a25709a29a4a26f1d7a30731
Have Samuel <samiehave9@gmail.com>
2023-03-02 01:17:21 +0300

    Writing Tests

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,11 +1,31 @@
 
 it('should calculate the monthly rate correctly', function () {
     // ...
+    const values = {
+      amount: 10000,
+      years:8,
+      rate:5.8
+    };
+    expect(calculateMonthlyPayment(values)).otEqual('130.63');
   });
   
   
   it("should return a result with 2 decimal places", function() {
     // ..
+    const values = {
+      amount: 10045,
+      years:8,
+      rate:5.8
+    };
+    expect(calculateMonthlyPayment(values)).otEqual('131.21');
   });
   
   /// etc
+  it('should handle terribly high rates', () => {
+    const values = {
+      amoutn: 1000,
+      years: 50,
+      rate: 100
+    };
+    expect(calculateMonthlyPayment(values)).toEqual('83.33')
+  })
