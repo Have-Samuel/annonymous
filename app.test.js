a29525b984a440804a7d457786067edff0c88d06
Have Samuel <samiehave9@gmail.com>
2023-03-01 04:29:35 +0300

    Arranging the tests with describe

--- app.test.js
+++ app.test.js
@@ -1,30 +1,36 @@
-it('Should calculate the high tax bracket', function() {
-    expect(calculateTax(50000)).toEqual(12500);
-    expect(calculateTax(100000)).toEqual(25000);
+describe('calculate tests', function() {
+    it('Should calculate the high tax bracket', function() {
+        expect(calculateTax(50000)).toEqual(12500);
+        expect(calculateTax(100000)).toEqual(25000);
+    })
+    
+    it('Should calculate the low tax bracket', function() {
+        expect(calculateTaxes(10000).toEqual(1500));
+        expect(calculateTaxes(100).toEqual(15));
+        expect(calculateTaxes(0).toEqual(0));
+    })
+    
+    it('Should reject invalid incomes', function() {
+        expect(() => calculateTaxes('addajdjd').toThrowError());
+        expect(() => calculateTaxes([]).toThrowError());
+        expect(() => calculateTaxes(true).toThrowError());
+    })
 })
 
-it('Should calculate the low tax bracket', function() {
-    expect(calculateTaxes(10000).toEqual(1500));
-    expect(calculateTaxes(100).toEqual(15));
-    expect(calculateTaxes(0).toEqual(0));
+describe('removeDupes tests', function() {
+    it('Should remove duplicates from an array', function() {
+        expect(removeDupes([1, 1, 2, 3])).toEqual([1, 2, 3]);
+        expect(removeDupes([1, 2, 3])).toBeInstanceOf(Array);
+    })
+    
+    it('Should remove duplicates from an string', function() {
+        expect(removeDupes('hello')).toEqual('helo');
+        expect(removeDupes('hello')).toBeInstanceOf(String);
+    })
 })
 
-it('Should reject invalid incomes', function() {
-    expect(() => calculateTaxes('addajdjd').toThrowError());
-    expect(() => calculateTaxes([]).toThrowError());
-    expect(() => calculateTaxes(true).toThrowError());
-})
-
-it('Should remove duplicates from an array', function() {
-    expect(removeDupes([1, 1, 2, 3])).toEqual([1, 2, 3]);
-    expect(removeDupes([1, 2, 3])).toBeInstanceOf(Array);
-})
-
-it('Should remove duplicates from an string', function() {
-    expect(removeDupes('hello')).toEqual('helo');
-    expect(removeDupes('hello')).toBeInstanceOf(String);
-})
-
-it('Should remove value from array', function() {
-    expect(remove([1, 2, 3], 2)).not.toContain(2);
+describe('remove tests', function() {
+    it('Should remove value from array', function() {
+        expect(remove([1, 2, 3], 2)).not.toContain(2);
+    })
 })
